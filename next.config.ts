0ed35507e158e4caf9fc71a61314253eda33eee1
Icecrack3r <lady.elena112@gmail.com>
2026-03-13 17:10:59 +0700

    fix: disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     remotePatterns: [
       {
