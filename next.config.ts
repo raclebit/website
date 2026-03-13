61baf23d1194c4022a6fab58d34fb5c5ca009d12
Icecrack3r <lady.elena112@gmail.com>
2026-03-13 23:48:05 +0700

    fix: enable standalone output for Docker build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
